aa6dae21551bd7797bcbffc71ad4ef976a7e25b2
Ahmed Elgaidi <49118737+AhmedElgaidi@users.noreply.github.com>
2020-02-28 18:13:44 +0200

    Add files via upload

---
+++ sandbox.js
@@ -0,0 +1,28 @@
+const correctAnswers = ['B', 'B', 'B', 'B'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+    userAnswers.forEach((answer, index) => {
+        if(answer === correctAnswers[index]) {
+            score += 25
+        }
+    })
+    //show the result
+
+    scrollTo(0, 0)// go to top with the help of windwo object
+    // animate the score
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+        result.classList.remove('d-none');
+        if(output === score) {
+            clearInterval(timer);
+        }else {
+            output++
+        }
+    },1)
+})
